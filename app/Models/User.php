603bd9342587ecf530d30927998f8af863c32bbf
yowxy <iklilblender@gmail.com>
2024-11-21 14:36:22 +0700

    wip: setup filament for dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory,SoftDeletes;
+    use HasFactory,Notifiable,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'profile_picture'
+        'picture'
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('profile_picture');
+            $table->string('picture');
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
